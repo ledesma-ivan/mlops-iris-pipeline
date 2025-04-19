09357a42ef140f6dea0809919c9bc231a0ab5196
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-04-19 20:46:12 -0300

    add requisitos openpyxl para compatibilidad con pandas.

--- src/ingest.py
+++ src/ingest.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import load_iris
+import pandas as pd
+
+# Cargo el dataset
 data = load_iris()
 
-name = 'iris_dataset.xslx'
-data.to_excel(name)
+# Convierto en dataframe
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+
+# Guardo en formato Excel
+name = 'iris_dataset.xlsx' 
+df.to_excel(name, index=False) 
